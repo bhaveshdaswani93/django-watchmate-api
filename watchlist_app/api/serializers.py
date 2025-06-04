ffd67ad449939eff7a529d786c11693980cc358b
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-06-04 17:58:56 +0530

    Add validation for movie name length in MovieSerializer

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -15,3 +15,8 @@ class MovieSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+    
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError("Name is too short")
+        return value
