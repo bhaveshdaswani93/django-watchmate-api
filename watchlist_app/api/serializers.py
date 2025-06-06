4ba7c753b7c3c860394133001e63a25a86ab8edc
Bhavesh Daswani <bhaveshdaswani93@gmail.com>
2025-06-06 13:54:09 +0530

    Refactor MovieSerializer to use ModelSerializer and add custom validation for name and description

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from watchlist_app.models import Movie
 
+
+
 def validate_min_length(value):
     min_length = 5
     if len(value) < min_length:
         raise serializers.ValidationError(f"Value must be at least {min_length} characters long")
     return value
-
+"""
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=100, validators=[validate_min_length])
@@ -31,3 +33,22 @@ class MovieSerializer(serializers.Serializer):
         if len(value) < 2:
             raise serializers.ValidationError("Name is too short")
         return value
+"""
+class MovieSerializer(serializers.ModelSerializer):
+    name_length = serializers.SerializerMethodField()
+    def get_name_length(self, obj):
+        return len(obj.name)
+    
+    class Meta:
+        model = Movie
+        fields = '__all__'
+        # exclude = ['id', 'active']
+        extra_kwargs = {
+            'name': {'validators': [validate_min_length]},
+            'description': {'required': False, 'allow_blank': True}
+        }
+    
+    def validate(self, attrs):
+        if attrs.get('name') == attrs.get('description'):
+            raise serializers.ValidationError("Name and description cannot be the same")
+        return super().validate(attrs)
